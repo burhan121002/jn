058549e8abe9bc4d6e9f644fbe4ba679eb502fd0
burhan121002 <106872441+burhan121002@users.noreply.github.com>
2024-01-15 22:42:52 +0530

    Create MyServlet.java

---
+++ MyServlet.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MyServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h2>Hello, this is my first servlet!</h2>");
+        out.println("</body></html>");
+    }
+}
